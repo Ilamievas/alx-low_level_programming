b748e4dac62113b187d5862115839d11c9d8363d
ilamievas <ilamievas@gmail.com>
2023-07-07 14:01:12 -0700

    binary

--- 1-print_binary.c
+++ 1-print_binary.c
@@ -2,28 +2,37 @@
 #include <stdio.h>
 
 /**
- * print_binary - print binary representation of a number
- * @n: decimal number to print as binary
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int temp;
-	int shifts;
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int leading_zeros = 1;
 
-	if (n == 0)
+	while (mask > 0)
 	{
-		printf("0");
-		return;
+		if ((n & mask) != 0)
+			leading_zeros = 0;
+		if (!leading_zeros)
+			printf("%d", (n & mask) != 0);
+		mask >>= 1;
 	}
-
-	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
-		;
-
-	for (; shifts >= 0; shifts--)
-	{
-		if ((n >> shifts) & 1)
-			printf("1");
-		else
-			printf("0");
-	}
+	if (leading_zeros)
+		prinft("0");
 }
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	unsigned long int num = 42;
+
+	printf("Binary representation of %lu: ", num);
+	print_binary(num);
+	printf("\n");
+	return (0);
+}}
